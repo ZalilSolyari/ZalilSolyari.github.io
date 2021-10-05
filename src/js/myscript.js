fc9fa96be3ef30c6eb21cce84147d1beffedeade
ZalilSolyari <89410794+ZalilSolyari@users.noreply.github.com>
2021-10-05 18:05:54 +0500

    На доработку

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,11 +1,15 @@
 
-let tip = prompt("Выберите тип сайта: 1 - Крутой 2 - Очень крутой 3 - Улётный", "Введите цифру");
+
+function calculated(){
+let tip = document.querySelector('#spisok1.1').value;
     console.log(tip);
 
-let des = prompt("Выберите дизайн сайта: 1 - Визитка 2 - Секретные архивы спец.служб 3 - Пентагон", "Введите цифру");
+let des = document.querySelector('#spisok1.2').value;
+
 console.log(des);
 
-let adapt = prompt("Выберите адаптиновсть сайта: 1 - Импровизируй 2 - Адаптируйся 3 - Выживай", "Введите цифру");
+let adapt = document.querySelector('#spisok1.3').value;
+
 console.log(adapt);
 
 let itog;
@@ -13,54 +17,198 @@ let srok;
 
 let num1 = tip;
 
-if(num1 === "1"){
+if(num1 === "opt2"){
   num1 = 1000;   
 
 console.log(num1);
 }
-else if(num1 === "2"){
+else if(num1 === "opt3"){
      num1 = 2000;
     console.log(num1);
 }
-else if(num1 === "3"){
+else if(num1 === "opt4"){
      num1 = 3000;
     console.log(num1);
 }
 let num2 = des;
 
 
-if(num2 === "1"){
+if(num2 === "opt2"){
   num2 = 1000;   
 
 console.log(num2);
 }
-else if(num2 === "2"){
+else if(num2 === "opt3"){
      num2 = 2000;
     console.log(num2);
 }
-else if(num2 === "3"){
+else if(num2 === "opt4"){
      num2 = 3000;
     console.log(num2);
 }
 let num3 = adapt;
 
-    if(num3 === "1"){
+    if(num3 === "opt2"){
   num3 = 1000;   
 
 console.log(num3);
 }
-else if(num3 === "2"){
+else if(num3 === "opt3"){
      num3 = 2000;
     console.log(num3);
 }
-else if(num3 === "3"){
+else if(num3 === "opt4"){
      num3 = 3000;
     console.log(num3);
 }
     
-itog = +num1 + +num2 + +num3+" драхм";
+itog = +num1 + +num2 + +num3+" арбузов";
 console.log(itog);
-    confirm(itog);
+   
 srok = +tip + +des + +adapt+" Дн.";
 console.log(srok);
-    confirm(srok);
+  
+    }
+    
+
+/*$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top + "px"});
+});
+*/
+
+    
+    $(window).scroll(() => {
+       let scrollDistance = $(window).scrollTop();
+        
+        $(".section").each((i, el) => {
+          
+            if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+                $("nav a").each((i, el) => {
+                   if($(el).hasClass("active")){
+                       $(el).removeClass("active");
+                   } 
+                });
+                $('nav li:eq('+ i +')').find('a').addClass('active');
+            };
+            
+        });
+        
+    });
+ 
+    
+
+
+
+
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+
+
+
+$(document).ready(function(){
+    showStartModal();
+});
+
+function showStartModal(){
+    setTimeout(function(){
+        $('#exampleModal').modal('show');
+    }, 10000);
+}
+
+let time = 2, begcifr1raz = 1;
+$(window).scroll(function(){
+    $('.statist').each(function(){
+        let
+        cPos = $(this).offset().top,
+            topWindow = $(window).scrollTop();
+        if (cPos < topWindow + 600){
+            
+            if(begcifr1raz < 2){
+                $('.statist').addClass('vid');
+            $('p').each(function(){
+                let
+                i = 1,
+                    num = $(this).data('num'),
+                    step = 1000 * time / num,
+                    that= $(this),
+                    int = setInterval(function(){
+                        if (i <= num){
+                            that.html(i);
+                        }
+                        else {
+                            begcifr1raz = begcifr1raz + 2;    
+                            clearInterval(int);
+                            
+                        }
+                        i++;
+                    },step);
+            });
+                };
+        };
+    });
+});
+
+
+
+   let options = {threshold: [1]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation1');
+    elements.each((i, el) => {
+       observer.observe(el);
+    });
+    
+    function onEntry (entry){
+        entry.forEach(change =>{
+            if(change.isIntersecting){
+                change.target.classList.add('show-animation1');
+            }
+        }) ;
+    }
+    
+    
+    
+      let options1 = {threshold: [0.5]};
+    let observer1= new IntersectionObserver(onEntry, options1);
+    let elements1 = $('.imgview');
+    elements.each((i, el) => {
+       observer1.observe(el);
+    });
+    
+    function onEntry1 (entry1){
+        entry1.forEach(change1 =>{
+            if(change1.isIntersecting){
+                change1.target.src = change1.target.dataset.src;
+            }
+        }) ;
+    }
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
